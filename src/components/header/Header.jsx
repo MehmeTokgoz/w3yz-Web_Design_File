4d84f6a099daa4eee896b20f8e87bcee7aecaa21
MEHMETTOKGOZ <42mt06@gmail.com>
2023-05-14 10:15:52 +0300

    navbar should be styled

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,56 +8,49 @@ function Header() {
     <header className="header">
       <div className="container">
         <div className="row">
-          <div className="col">
-            <div className="row">
-              <div className="col d-flex flex-row justify-content-left align-items-center ">
-                <a
-                  className="logo-anchor"
-                  href="#"
-                >
-                  <img className="logo-warm" src={Logo} alt="" />
-                  <img className="happy-galery" src={Logo1} alt="" />
-                </a>
+          <div className="col d-flex flex-row justify-content-left align-items-center ">
+            <a className="logo-anchor" href="#">
+              <img className="logo-warm" src={Logo} alt="" />
+              <img className="happy-galery" src={Logo1} alt="" />
+            </a>
+          </div>
+          <div className="col d-flex justify-content-around align-items-center">
+            <nav className="navbar">
+              <div>
+                <ul className="navbar-nav d-flex flex-row justify-content-around">
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      Hakkımızda
+                    </a>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      SIkça SOrulan Sorular
+                    </a>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      Galeri
+                    </a>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      İletişim
+                    </a>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      Giriş Yap
+                    </a>
+                  </li>
+                  <li className="nav-item mx-2">
+                    <a href="" className="nav-link nav-link-texts">
+                      Alışveriş (0)
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <div className="col d-flex justify-content-around align-items-center">
-                <nav className="navbar">
-                  <div>
-                    <ul className="navbar-nav d-flex flex-row justify-content-around">
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          Hakkımızda
-                        </a>
-                      </li>
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          SIkça SOrulan Sorular
-                        </a>
-                      </li>
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          Galeri
-                        </a>
-                      </li>
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          İletişim
-                        </a>
-                      </li>
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          Giriş Yap
-                        </a>
-                      </li>
-                      <li className="nav-item mx-2">
-                        <a href="" className="nav-link nav-link-texts">
-                          Alışveriş (0)
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-            </div>
+            </nav>
           </div>
         </div>
       </div>
@@ -65,3 +58,65 @@ function Header() {
   );
 }
 export default Header;
+
+/**
+ * 
+ *     // <header className="header">
+    //   <div className="container">
+    //     <div className="row">
+    //       <div className="col">
+    //         <div className="row">
+    //           <div className="col d-flex flex-row justify-content-left align-items-center ">
+    //             <a
+    //               className="logo-anchor"
+    //               href="#"
+    //             >
+    //               <img className="logo-warm" src={Logo} alt="" />
+    //               <img className="happy-galery" src={Logo1} alt="" />
+    //             </a>
+    //           </div>
+    //           <div className="col d-flex justify-content-around align-items-center">
+    //             <nav className="navbar">
+    //               <div>
+    //                 <ul className="navbar-nav d-flex flex-row justify-content-around">
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       Hakkımızda
+    //                     </a>
+    //                   </li>
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       SIkça SOrulan Sorular
+    //                     </a>
+    //                   </li>
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       Galeri
+    //                     </a>
+    //                   </li>
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       İletişim
+    //                     </a>
+    //                   </li>
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       Giriş Yap
+    //                     </a>
+    //                   </li>
+    //                   <li className="nav-item mx-2">
+    //                     <a href="" className="nav-link nav-link-texts">
+    //                       Alışveriş (0)
+    //                     </a>
+    //                   </li>
+    //                 </ul>
+    //               </div>
+    //             </nav>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </header>
+ * 
+ */
